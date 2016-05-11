2ab3a830ec8d8301ea98deb6e93edfa1a164249f
Dave <davekennewell@gmail.com>
2016-05-11 09:31:19 +0400

    from chennaign - added models and dbrouter (not working yet)

--- twitter/views.py
+++ twitter/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.conf import settings
 import tweepy, json
 from tweepy import OAuthHandler
-from models import Tweet
+from chennaign.twitter.models import Tweet
+from dateutil import parser
 
 auth = OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
 auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_SECRET)
@@ -28,7 +29,7 @@ def json_geo_tweets(request):
     # Get a list of tweets and convert them to json so we can parse the geo-enabled ones
     tweets = []
     count = 0
-    for tweet in tweepy.Cursor(api.search,  q='%23flood').items(1000):
+    for tweet in tweepy.Cursor(api.search,  q='#rain').items(500):
         count += 1
         tweets.append(json.dumps(tweet._json, indent=2, sort_keys=True))
 
@@ -52,13 +53,74 @@ def json_geo_tweets(request):
                      "created_at": tweet['created_at'],
                      "id_str": tweet['id_str']
                 }
-
             }
             geo_tweets['features'].append(geo_json_feature)
 
             #write to models
-            Tweet.id_str = tweet['id_str']
-            Tweet.text = tweet['text']
+            try:
+                Tweet.objects.create(
+                    id_str=tweet['id_str'],
+                    text=tweet['text'],
+                    created_at=parser.parse(tweet['created_at']),
+
+                    coordinates_lon = tweet['coordinates'].get('coordinates')[0],
+                    coordinates_lat = tweet['coordinates'].get('coordinates')[1]
+                    # coordinates_type = tweet['id_str'],
+
+                    # entities_media_id_str = tweet['id_str'],
+                    # entities_media_media_url = tweet['id_str'],
+                    # entities_media_media_url_https = tweet['id_str'],
+                    # entities_media_url = tweet['id_str'],
+                    # entities_media_display_url = tweet['id_str'],
+                    # entities_media_expanded_url = tweet['id_str'],
+                    # entities_media_sizes_w = tweet['id_str'],
+                    # entities_media_sizes_h = tweet['id_str'],
+                    # entities_media_sizes_resize = tweet['id_str'],
+                    # entities_media_type = tweet['id_str'],
+                    # entities_media_indices = tweet['id_str'],
+                    #
+                    # entities_urls_url = tweet['id_str'],
+                    # entities_urls_display_url = tweet['id_str'],
+                    # entities_urls_expanded_url = tweet['id_str'],
+                    # entities_urls_indices = tweet['id_str'],
+                    #
+                    # entities_user_mentions_id_str = tweet['id_str'],
+                    # entities_user_mentions_screen_name = tweet['id_str'],
+                    # entities_user_mentions_name = tweet['id_str'],
+                    # entities_user_mentions_indices = tweet['id_str'],
+                    #
+                    # entities_hastags_text = tweet['id_str'],
+                    # entities_hastags_indices = tweet['id_str'],
+                    #
+                    # entities_symbols_text = tweet['id_str'],
+                    # entities_symbols_indices = tweet['id_str'],
+                    #
+                    # favorite_count = tweet['id_str'],
+                    # filter_level = tweet['id_str'],
+                    #
+                    # lang = tweet['id_str'],
+                    #
+                    # place_attibutes_street_address = tweet['id_str'],
+                    # place_attibutes_locality = tweet['id_str'],
+                    # place_attibutes_region = tweet['id_str'],
+                    # place_attibutes_iso3 = tweet['id_str'],
+                    # place_attibutes_postal_code = tweet['id_str'],
+                    # place_attibutes_phone = tweet['id_str'],
+                    # place_attibutes_twitter = tweet['id_str'],
+                    # place_attibutes_url = tweet['id_str'],
+                    # place_attibutes_app_id = tweet['id_str'],
+                    #
+                    # place_bounding_box = tweet['id_str'],
+                    # place_country = tweet['id_str'],
+                    # place_country_code = tweet['id_str'],
+                    # place_full_name = tweet['id_str'],
+                    # place_id = tweet['id_str'],
+                    # place_name = tweet['id_str'],
+                    # place_place_type = tweet['id_str'],
+                    # place_url = tweet['id_str']
+                )
+            except:
+                pass
 
 
     geo_tweets = json.dumps(geo_tweets, indent=2)
