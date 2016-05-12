4b726f4f4975375ef8422a91d19d0615c293dfb7
Dave <davekennewell@gmail.com>
2016-05-12 12:08:39 +0400

    working with script in view - need to get the script into a managment command though.

--- geonodetwitter/views.py
+++ geonodetwitter/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from django.conf import settings
 import tweepy, json
 from tweepy import OAuthHandler
-from chennaign.twitter.models import Tweet
+from geonodetwitter.models import Tweet
 from dateutil import parser
+from django.contrib.gis.geos import GEOSGeometry
 
 auth = OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
 auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_SECRET)
@@ -29,7 +30,7 @@ def json_geo_tweets(request):
     # Get a list of tweets and convert them to json so we can parse the geo-enabled ones
     tweets = []
     count = 0
-    for tweet in tweepy.Cursor(api.search,  q='#rain').items(500):
+    for tweet in tweepy.Cursor(api.search,  q='#rain').items(100):
         count += 1
         tweets.append(json.dumps(tweet._json, indent=2, sort_keys=True))
 
@@ -39,12 +40,12 @@ def json_geo_tweets(request):
         "features": []
     }
 
-    # parse each tweet to see if it has a 'place' attribute. If it does, then add it to geo_tweets
+    # parse each tweet to see if it has a 'coordinate' attribute. If it does, then add it to geo_tweets
     for line in tweets:
-        tweet=json.loads(line)
+        tweet = json.loads(line)
         if tweet['coordinates']:
 
-            #create json
+            # create json for preview map
             geo_json_feature = {
                 "type": "Feature",
                 "geometry": tweet['coordinates'],
@@ -56,19 +57,23 @@ def json_geo_tweets(request):
             }
             geo_tweets['features'].append(geo_json_feature)
 
-            #write to models
+            # write to models for geonode map
+            temp_point = 'POINT(%s %s)' % (tweet['coordinates'].get('coordinates')[0],
+                                           tweet['coordinates'].get('coordinates')[1]
+                                           )
             try:
                 Tweet.objects.create(
                     id_str=tweet['id_str'],
                     text=tweet['text'],
                     created_at=parser.parse(tweet['created_at']),
+                    coordinates_lon=tweet['coordinates'].get('coordinates')[0],
+                    coordinates_lat=tweet['coordinates'].get('coordinates')[1],
+                    point=GEOSGeometry(temp_point)
 
-                    coordinates_lon = tweet['coordinates'].get('coordinates')[0],
-                    coordinates_lat = tweet['coordinates'].get('coordinates')[1]
                     # coordinates_type = tweet['id_str'],
 
                     # entities_media_id_str = tweet['id_str'],
-                    # entities_media_media_url = tweet['id_str'],
+                    # media_url = tweet['media']['media_url']
                     # entities_media_media_url_https = tweet['id_str'],
                     # entities_media_url = tweet['id_str'],
                     # entities_media_display_url = tweet['id_str'],
@@ -121,7 +126,6 @@ def json_geo_tweets(request):
                 )
             except:
                 pass
-
 
     geo_tweets = json.dumps(geo_tweets, indent=2)
 
